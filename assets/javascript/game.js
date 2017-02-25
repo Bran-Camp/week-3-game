97371c3995e160da27b607743652e6ded0dacb3a
Brandon Campbell <Brandon Campbell>
2017-02-24 22:52:51 -0600

    fifth commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var wordList = [
+"peanuts",
+"candy",
+"fruits",
+"cake",
+"steak"
+]
+
+var chosenWord = "";
+var letterInChosenWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongGuesses = [];
+
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 9;
+
+function startGame(){
+
+}
+
+function checkLetters(letter){
+
+}
+
+function roundComplete(){
+
+}
+
+document.onkeyup = function(event){
+	
+}
